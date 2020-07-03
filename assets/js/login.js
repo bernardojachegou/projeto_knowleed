eea954510a1a51ff198bf5929c3a2b7f31d155fa
William Goncalves <euwilliamgoncalves@gmail.com>
2020-07-03 20:52:43 -0300

    Incluindo demonstração do modal de quiz

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,6 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const modal = document.getElementById('modal-quiz');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
