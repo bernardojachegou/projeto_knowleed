c038f85d217c649374f3996fe776a66032b77824
NayraCruz <nayra.ncruz@gmail.com>
2020-07-04 11:58:12 -0300

    início de um script para o menu mobile que precisa ser melhorado

---
+++ assets/js/perfil.js
@@ -0,0 +1,9 @@
+function toggleDiv() {
+    var navDesktop = document.getElementById("nav-desktop");
+
+    if (navDesktop.style.display === "none") {
+        navDesktop.style.display = "block";
+    } else {
+        navDesktop.style.display = "none";
+    }
+}
