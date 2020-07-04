2d5fb1ad61262f6bc8b6b0befead9a0d3a801818
William Goncalves <euwilliamgoncalves@gmail.com>
2020-07-04 16:51:28 -0300

    Iniciando servidor back-end, reorganizando estruturas de pastas e iniciando refatoração

---
+++ src/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const server = express();
+
+// configurar pasta pública
+
+server.use(express.static("public"));
+
+// habilitar req.body
+
+server.use(express.urlencoded({extended: true}));
+
+// utilizando template engine
+
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurando caminhos da minha aplicação
+
+// página inicial
+// req: requisição
+// res: resposta
+
+server.get("/", (req, res) => {
+    return res.render("index.html");
+});
+
+server.get("/perfil", (req, res) => {
+    return res.render("perfil.html");
+});
+
+server.get("/livros", (req, res) => {
+    return res.render("livros.html");
+});
+
+server.get("/leitura", (req, res) => {
+    return res.render("leitura.html");
+});
+
+server.get("/review", (req, res) => {
+    return res.render("review.html");
+});
+
+server.get("/ranking", (req, res) => {
+    return res.render("ranking.html");
+});
+
+server.get("/conquistas", (req, res) => {
+    return res.render("conquistas.html");
+});
+
+// ligando o servidor
+
+server.listen(3000);
