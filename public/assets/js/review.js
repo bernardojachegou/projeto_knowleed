b351ccd6fdfb451fb379a11d9f218d89faaf8499
William Goncalves <euwilliamgoncalves@gmail.com>
2020-07-05 01:32:45 -0300

    Finalizando página de review

--- public/assets/js/review.js
+++ public/assets/js/review.js
@@ -1,27 +1,42 @@
-const writeModal = document.querySelector("#modalwrite")
-document.querySelector(".content a")
-.addEventListener("click", function(){
-    writeModal.classList.remove("hide")
-})
-document.querySelector("#modalwrite i")
-.addEventListener("click", function(){
-    writeModal.classList.add("hide")
-})
-const myReviews = document.querySelector("#myreviews")
-document.querySelector(".mywrites")
-.addEventListener("click", function(){
-  if(myReviews.classList.contains("hidewrite")){
-      myReviews.classList.remove("hidewrite")
-  }else{
-      myReviews.classList.add("hidewrite")
-  }
-})
-const otherReviews = document.querySelector("#otherreviews")
-document.querySelector(".otherwrites")
-.addEventListener("click", function(){
-    if(otherReviews.classList.contains("hidewrite")){
-        otherReviews.classList.remove("hidewrite")
-    }else{
-        otherReviews.classList.add("hidewrite")
-    }
-})
+const abreReviews = document.getElementById('review-btn');
+const abreReviewsDois = document.getElementById('review-btn-dois');
+const fechaReviews = document.getElementById('close-review-btn');
+const modalReviews = document.getElementById('modal-reviews');
+
+fechaReviews.addEventListener('click', () => {
+	modalReviews.classList.add("hidden");
+});
+
+abreReviews.addEventListener('click', () => {
+	modalReviews.classList.remove("hidden");
+});
+
+abreReviewsDois.addEventListener('click', () => {
+	modalReviews.classList.remove("hidden");
+});
+
+const abreWrite = document.getElementById('write-btn');
+const abreWriteDois = document.getElementById('write-btn-dois');
+const fechaWrite = document.getElementById('close-write-btn');
+const modalWrite = document.getElementById('modal-write');
+const enviaWrite = document.getElementById('send-write');
+const sucessoWrite = document.getElementById('send-success');
+const textoWrite = document.getElementById('send-text');
+
+fechaWrite.addEventListener('click', () => {
+	modalWrite.classList.add("hidden");
+});
+
+abreWrite.addEventListener('click', () => {
+	modalWrite.classList.remove("hidden");
+	textoWrite.classList.remove("hidden");
+});
+
+abreWriteDois.addEventListener('click', () => {
+	modalWrite.classList.remove("hidden");
+});
+
+enviaWrite.addEventListener('click', () => {
+	sucessoWrite.classList.remove("hidden");
+	textoWrite.classList.add("hidden");
+});
