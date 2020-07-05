1234251ef15befa39083b4bf23fcc747a67270df
William Goncalves <euwilliamgoncalves@gmail.com>
2020-07-05 16:08:47 -0300

    Finalizando página de conquistas

--- public/assets/js/conquistas.js
+++ public/assets/js/conquistas.js
@@ -1,29 +1,23 @@
-const read = document.querySelector("#reading")
-document.querySelector(".read")
-.addEventListener("click", function(){
-    if(read.classList.contains("hideread")){
-        read.classList.remove("hideread")
-    }else{
-        read.classList.add("hideread")
-    }
-})
+const abreRewards = document.getElementById('rewards-btn');
+const fechaRewards = document.getElementById('close-rewards-btn');
+const modalRewards = document.getElementById('modal-rewards');
 
-const write = document.querySelector("#writing")
-document.querySelector(".write")
-.addEventListener("click", function(){
-    if(write.classList.contains("hidewriting")){
-        write.classList.remove("hidewriting")
-    }else{
-        write.classList.add("hidewriting")
-    }
-})
+fechaRewards.addEventListener('click', () => {
+	modalRewards.classList.add("hidden");
+});
 
-const quizz = document.querySelector("#quizz")
-document.querySelector(".quiz")
-.addEventListener("click", function(){
-    if(quizz.classList.contains("hidequizz")){
-        quizz.classList.remove("hidequizz")
-    }else{
-        quizz.classList.add("hidequizz")
-    }
-})
+abreRewards.addEventListener('click', () => {
+	modalRewards.classList.remove("hidden");
+});
+
+const abrePoints = document.getElementById('points-btn');
+const fechaPoints = document.getElementById('close-points-btn');
+const modalPoints = document.getElementById('modal-points');
+
+fechaPoints.addEventListener('click', () => {
+	modalPoints.classList.add("hidden");
+});
+
+abrePoints.addEventListener('click', () => {
+	modalPoints.classList.remove("hidden");
+});
