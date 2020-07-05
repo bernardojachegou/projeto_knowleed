097b0c1148d6bc6c2d1c1e9b592596cd92df8089
William Goncalves <euwilliamgoncalves@gmail.com>
2020-07-04 21:47:53 -0300

    Finalizando página de perfil e iniciando página de livros

--- public/assets/js/livros.js
+++ public/assets/js/livros.js
@@ -1,11 +1,35 @@
-const abreModal = document.getElementById('modal-btn');
-const fechaModal = document.getElementById('close-btn');
-const modal = document.getElementById('modal-quiz');
+const abreSinopse = document.getElementById('sinopse-btn');
+const fechaSinopse = document.getElementById('close-sinopse-btn');
+const modalSinopse = document.getElementById('modal-sinopse');
 
-fechaModal.addEventListener('click', () => {
-	modal.classList.add("hidden");
+fechaSinopse.addEventListener('click', () => {
+	modalSinopse.classList.add("hidden");
 });
 
-abreModal.addEventListener('click', () => {
-	modal.classList.remove("hidden");
+abreSinopse.addEventListener('click', () => {
+	modalSinopse.classList.remove("hidden");
+});
+
+const abreReviews = document.getElementById('review-btn');
+const fechaReviews = document.getElementById('close-review-btn');
+const modalReviews = document.getElementById('modal-reviews');
+
+fechaReviews.addEventListener('click', () => {
+	modalReviews.classList.add("hidden");
+});
+
+abreReviews.addEventListener('click', () => {
+	modalReviews.classList.remove("hidden");
+});
+
+const abreQuiz = document.getElementById('modal-btn');
+const fechaQuiz = document.getElementById('close-btn');
+const modalQuiz = document.getElementById('modal-quiz');
+
+fechaQuiz.addEventListener('click', () => {
+	modalQuiz.classList.add("hidden");
+});
+
+abreQuiz.addEventListener('click', () => {
+	modalQuiz.classList.remove("hidden");
 });
